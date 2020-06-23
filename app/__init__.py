7916de9bb75e9e79c063ac0047c977133e5a6bdb
cmoch <65452402+cmoch@users.noreply.github.com>
2020-06-23 17:27:17 -0400

    UPDATES FROM REPO

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
-
-# app/__init__.py
+# web_app/__init__.py
 
 import os
 from dotenv import load_dotenv
+from flask import Flask
+
+from web_app.routes.home_routes import home_routes
+from web_app.routes.weather_routes import weather_routes
 
 load_dotenv()
 
-APP_ENV = os.getenv("APP_ENV", default="development") # use "production" on a remote server
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.register_blueprint(home_routes)
+    app.register_blueprint(weather_routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,23 +1,26 @@
-import os
-from dotenv import load_dotenv
-from flask import Flask
+# web_app/routes/home_routes.py
 
-from web_app.routes.home_routes import home_routes
-#from web_app.routes.weather_routes import weather_routes
+from flask import Blueprint, render_template, redirect, request, flash
 
-load_dotenv()
+# ...
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
+@home_routes.route("/users/new")
+def register():
+    print("VISITED THE REGISTRATION PAGE")
+    return render_template("new_user_form.html")
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = SECRET_KEY
+@home_routes.route("/users/create", methods=["POST"])
+def create_user():
+    print("FORM DATA:", dict(request.form))
+    # FYI: we are able to access the form data via the "request" object we import from flask
+    # ... these keys correspond with the "name" attributes of each <input> element in the form!
+    #> {'full_name': 'Example User', 'email_address': 'me@example.com', 'country': 'US'}
 
-    app.register_blueprint(home_routes)
-    #app.register_blueprint(weather_routes)
+    user = dict(request.form)
+    # todo: store in a database or google sheet!
 
-    return app
-
-if __name__ == "__main__":
-    my_app = create_app()
-    my_app.run(debug=True)
+    # FYI: "warning", "primary", "danger", "success", etc. are bootstrap color classes
+    # ... see https://getbootstrap.com/docs/4.3/components/alerts/
+    # ... and the flash messaging section of the "bootstrap_layout.html" file for more details
+    flash(f"User '{user['full_name']}' created successfully! (TODO)", "warning")
+    return redirect("/")
